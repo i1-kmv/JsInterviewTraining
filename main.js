9deb5a7f546d0b6c53152824139c1b35aec6ecab
i1-kmv <i1-kmv@mail.ru>
2021-01-22 22:30:00 +0300

    4less.1stque

--- main.js
+++ main.js
@@ -1,17 +1,17 @@
 // функция принимает массив и число, проверить, что в массиве есть два числа сумма которых равна переданному числы
-const getSummNumber = (arr, b) => {
+// const getSummNumber = (arr, b) => {
   
-  for (let n = 0; n < arr.length-1; n++){
-    for (let i = n + 1; i < arr.length; i++) {
-      if ((arr[n] + arr[i]) === b) {
-        return true
-      } 
-    }
-  }
+//   for (let n = 0; n < arr.length-1; n++){
+//     for (let i = n + 1; i < arr.length; i++) {
+//       if ((arr[n] + arr[i]) === b) {
+//         return true
+//       } 
+//     }
+//   }
   
-  return false
+//   return false
   
-}
+// }
 
 //console.log(getSummNumber([1,2,3,4,9,16],5))
 
@@ -19,9 +19,9 @@ const getSummNumber = (arr, b) => {
 
 // В функцию передается число, вернуть сумму всех цифр данно числа
 
-const getSumm = (num) => {
-  return String(num).split('').map(el => Number(el)).reduce((acc, el) => acc + el)
-}
+// const getSumm = (num) => {
+//   return String(num).split('').map(el => Number(el)).reduce((acc, el) => acc + el)
+// }
 
 //console.log(getSumm(11213))
 
@@ -29,12 +29,12 @@ const getSumm = (num) => {
 
 // Функция func принимае массив числе, необходимо реализовать функцию так, чтобы она вернула количество элементов переданного массива, которые отличны от наибольшего элемента не более чем на 10%
 
-function func(arr) {
+// function func(arr) {
   //let max = arr.sort((a,b) => a - b)[arr.length-1]
   //let max = Math.max(...arr)
-  let max = Math.max.apply(null, arr)
-  return arr.filter(el => max/el < 1.1 && el != max)
-}
+//   let max = Math.max.apply(null, arr)
+//   return arr.filter(el => max/el < 1.1 && el != max)
+// }
 
 //console.log(func([25,24,23,22,21,20]))
 
@@ -42,16 +42,16 @@ function func(arr) {
 
 //В функцию передается массив и целое число, необходимо заполнить массив нулями до указанного параметра.
 
-function func1(arr, len) {
-  if (arr.length < len) {
-    for (let i = arr.length; i < len; i++) {
-      arr[i] = 0
-    }
-  } else {
-    return 'Think bro'
-  }
-  return arr
-}
+// function func1(arr, len) {
+//   if (arr.length < len) {
+//     for (let i = arr.length; i < len; i++) {
+//       arr[i] = 0
+//     }
+//   } else {
+//     return 'Think bro'
+//   }
+//   return arr
+// }
 
 //console.log(func1([1,2,3], 8))
 
@@ -59,10 +59,10 @@ function func1(arr, len) {
 
 //Реализовать функцию unique, которая будет принимать массив чисел и возвращать массив очищенный от дублей
 
-function getUniqueNumbers(arr) {
+// function getUniqueNumbers(arr) {
   //return arr.sort((a,b) => a - b).filter((el,index, array) => array[index] != array[index-1])
-  return Array.from(new Set(arr))
-} 
+//   return Array.from(new Set(arr))
+// } 
 
 //console.log(getUniqueNumbers([1,6,2,3,4,5,1,2,5,5,16,8,4]))
 
@@ -70,13 +70,13 @@ function getUniqueNumbers(arr) {
 
 // Возвращать ближайшее целое число к тому что было передано, без остатка делящаяся на 5
 
-function round5(val) {
-  return Math.round(val / 5) * 5;
-}
+// function round5(val) {
+//   return Math.round(val / 5) * 5;
+// }
 
-console.log(round5(11))
-console.log(round5(2))
-console.log(round5(3))
+// console.log(round5(11))
+// console.log(round5(2))
+// console.log(round5(3))
 
 
 //=======================================================================
@@ -84,22 +84,33 @@ console.log(round5(3))
 
 //Необходимо реализовать функцию uniquePoints, которая принимает массив с координатами точек и возвращает массив, очищенный от дублей точек
 
-const arrPoints = [
-  {x = 5, у = 10},
-  {x = 6, y = 3},
-  {x = 3, y = 4},
-  {x = 5, у = 10},
-  {x = 6, y = 3},
-]
+// const arrPoints = [
+//   {x = 5, у = 10},
+//   {x = 6, y = 3},
+//   {x = 3, y = 4},
+//   {x = 5, у = 10},
+//   {x = 6, y = 3},
+// ]
 
-function uniquePoints(arr) {
-  let result = [arr[0]]
+// function uniquePoints(arr) {
+//   let result = [arr[0]]
 
-  for (let i = 0; i < arr. length; i==) {
-    if (!result.some(val => arr[i].x === val.x && arr[i].y === val.y)) {
-      result.push(arr[i])
-    }
-  }
+//   for (let i = 0; i < arr. length; i==) {
+//     if (!result.some(val => arr[i].x === val.x && arr[i].y === val.y)) {
+//       result.push(arr[i])
+//     }
+//   }
+// }
+
+//console.log(uniquePoints(arrPoints ))
+
+//=======================================================================
+
+//Необходимо перебрать массив с конца в начало и вывести сообщаения формата : под индексом 5 лежит значени 35
+
+let arrVal = [51, 8, 99, 71, 2, 35]
+
+
+for ( i = (arrVal.length - 1); i >= 0; i--) {
+  console.log(`Под индексом ${i} лежит элемент со значением ${arrVal[i]}`)
 }
-
-console.log(uniquePoints(arrPoints))
